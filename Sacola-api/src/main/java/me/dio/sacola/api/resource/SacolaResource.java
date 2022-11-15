abc5be41719f503b6d29cac53222739415c4a94f
thiago-dev <thiagosantosjss@gmail.com>
2022-11-15 17:55:06 -0300

    Resource

--- Sacola-api/src/main/java/me/dio/sacola/api/resource/SacolaResource.java
+++ Sacola-api/src/main/java/me/dio/sacola/api/resource/SacolaResource.java
@@ -1,5 +1,27 @@
 package me.dio.sacola.api.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import me.dio.sacola.api.model.Item;
+import me.dio.sacola.api.service.SacolaService;
+
+@RestController
+@RequestMapping("/ifood-devweek/sacolas")
 public class SacolaResource {
+	
+	@Autowired
+	private SacolaService sacolaService;
+	
+	@PostMapping
+	public Item incluirItemNaSacola(@RequestBody ItemDto itemDto) {
+		
+		return sacolaService.incluirItemNaSacola(itemDto);
+		
+	}
 
 }
