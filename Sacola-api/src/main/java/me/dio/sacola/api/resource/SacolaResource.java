0959541137fedbcc3570b389eac2caaa815d502d
thiago-dev <thiagosantosjss@gmail.com>
2022-11-15 19:31:07 -0300

    Fechar Sacola

--- Sacola-api/src/main/java/me/dio/sacola/api/resource/SacolaResource.java
+++ Sacola-api/src/main/java/me/dio/sacola/api/resource/SacolaResource.java
@@ -2,10 +2,12 @@ package me.dio.sacola.api.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -32,5 +34,16 @@ public class SacolaResource {
 		
 		return sacolaService.verSacola(id);
 	}
+	
+	@PatchMapping("/fecharSacola/{sacolaId}")
+	public Sacola fecharSacola(@PathVariable("sacolaId") Long sacolaId, 
+							   @RequestParam("formaPagamento") int formaPagamento) {
+		
+		return sacolaService.fecharSacola(sacolaId, formaPagamento);
+	}
+	
+	
+	
+	
 
 }
